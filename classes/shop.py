8758d5926d099e6a233316248a26c97160aa506d
MrLerich <93153582+MrLerich@users.noreply.github.com>
2022-09-03 12:17:48 +0300

    add shop and store

--- classes/shop.py
+++ classes/shop.py
@@ -1,43 +1,20 @@
 from typing import Dict
 
-from classes.abstract_storage import AbstractStorage
 from classes.base_storage import BaseStorage
+from exceptions import TooManyDifferentProducts
 
 
 class Shop(BaseStorage):
-    def __init__(self,items: Dict,  quantity: int = 20):
+    def __init__(self, items: Dict, quantity: int = 20):
         super().__init__(items, quantity)
 
-    def __repr__(self):
-        return f"{self.items}: {self.quantity}"
+    # def __repr__(self):
+    #     return f"{self.items}: {self.quantity}"
 
+    def add(self, name: str, amount: int):
+        # может быть в магазиен 5 уник товаров
+        if self.get_unique_items_count() >= 5:
+            raise TooManyDifferentProducts  # ошибка слишком много униктоваров
 
-    def add(self, title: str, quantity: int):
-        if self._items.get(title):
-            self._items[title] += quantity
-        else:
-            self._items[title] = quantity
+        super().add(name, amount) #остальное как в родительксом классе
 
-    def remove(self, title: str, quantity: int):
-        self._items[title] -= quantity
-
-    def get_free_space(self):
-        busy_space = 0
-        for item in self._items.items():
-            busy_space += item[1]
-
-        return self._capacity - busy_space
-
-    def get_items(self):
-        """"""
-        return self._items
-
-    def get_unique_items_count(self):
-        return len(self._items)
-
-    def check_add(self, title: str, quantity: int):
-        if ((self.get_unique_items_count() < 6 or title in self._items.keys())
-                and self.get_free_space() >= quantity):
-            return True
-        else:
-            return False
--- classes/store.py
+++ classes/store.py
@@ -1,6 +1,5 @@
 from typing import Dict
 
-from classes.abstract_storage import AbstractStorage
 from classes.base_storage import BaseStorage
 
 
@@ -9,50 +8,7 @@ class Store(BaseStorage):
     def __init__(self, items: Dict, quantity: int = 100):
         super().__init__(items, quantity)
 
-        def __repr__(self):
-            return f"{self.items}: {self.quantity}"
+    # def __repr__(self):
+    #     return f"{self.items}: {self.quantity}"
 
-    @property
-    def items(self):
-        return self._items
 
-    @property
-    def capacity(self):
-        return self._capacity
-
-    def add(self, title, quantity):
-        """Увеличивает запас items с учетом лимита quantity"""
-        if self._items.get(title):
-            self._items[title] += quantity
-        else:
-            self._items[title] = quantity
-
-    def remove(self, title, quantity):
-        self._items[title] -= quantity
-
-    def get_free_space(self):
-        busy_space = 0
-        if len(self._items) > 0:
-            for item in self._items.items():
-                busy_space += item[1]
-            return self._capacity - busy_space
-        else:
-            return self._capacity
-
-    def get_items(self):
-        return self._items
-
-    def get_unique_items_count(self):
-        return len(self._items)
-
-    def check_remove(self, title, quantity):
-        if self._items.get(title) and self._items[title] >= quantity:
-            return True
-        else:
-            return False
-
-    def check_add(self, quantity):
-        if self.get_free_space() >= quantity:
-            return True
-        else:
-            return False
